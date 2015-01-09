9d5a41980c747ca1eed1758969e65f6ae60ac0a4
love-somnus <928200207@qq.com>
2015-01-09 12:01:57 +0800

    version1.1.1

--- src/main/java/com/somnus/ExampleClient.java
+++ src/main/java/com/somnus/ExampleClient.java
@@ -1,13 +1,42 @@
 package com.somnus;
-
-import java.util.ArrayList;
-
-
+/*
+ * <sql id="Example_Where_Clause" >
+    <where >
+      <foreach collection="oredCriteria" item="criteria" separator="or" >
+        <if test="criteria.valid" >
+          <trim prefix="(" suffix=")" prefixOverrides="and" >
+            <foreach collection="criteria.criteria" item="criterion" >
+              <choose >
+                <when test="criterion.noValue" >
+                  and ${criterion.condition}
+                </when>
+                <when test="criterion.singleValue" >
+                  and ${criterion.condition} #{criterion.value}
+                </when>
+                <when test="criterion.betweenValue" >
+                  and ${criterion.condition} #{criterion.value} and #{criterion.secondValue}
+                </when>
+                <when test="criterion.listValue" >
+                  and ${criterion.condition}
+                  <foreach collection="criterion.value" item="listItem" open="(" close=")" separator="," >
+                    #{listItem}
+                  </foreach>
+                </when>
+              </choose>
+            </foreach>
+          </trim>
+        </if>
+      </foreach>
+    </where>
+  </sql>
+ *
+ */
 public class ExampleClient {
 
 	public static void main(String[] args) {
 		Example exampel = new Example();
 		exampel.or().andBankCodeBetween("1", "9").
-		andBankCodeEqualTo("a").andBankCodeIn(new ArrayList<String>());
+		andBankCodeEqualTo("a");
+		exampel.setOrderByClause("tran_date");
 	}
 }
