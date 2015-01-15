6df4c6631e173787ea336260bcdf192a090f7365
love-somnus <928200207@qq.com>
2015-01-15 12:45:23 +0800

    version1.1.1

--- src/main/java/com/somnus/StringFormat.java
+++ src/main/java/com/somnus/StringFormat.java
@@ -18,13 +18,11 @@ public class StringFormat {
 	 */
 	@Test
 	public void test1(){
-	 String str=null;
-	    str=String.format("Hi, %s", "Somnus"); // 格式化字符串
-	    System.out.println(str); // 输出字符串变量str的内容
-	    System.out.printf("3>7的结果是：%b %n", 3>7);
-	    System.out.printf("100的一半是：%d %n", 100/2);
-	    System.out.printf("50元的书打8.5折扣是：%f 元%n", 50*0.85);
-	    System.out.printf("上面的折扣是%d%% %n", 85);
+	    System.out.println(String.format("Hi, [%s]", "Somnus"));
+	    System.out.println(String.format("3>7的结果是：%b%n", 3>7));
+	    System.out.println(String.format("100的一半是：%d%n", 100/2));
+	    System.out.println(String.format("50元的书打8.5折扣是：%f元%n", 50*0.85));
+	    System.out.println(String.format("上面的折扣是%d%% %n", 85));
 	}
 	/*
 	2、常见日期时间格式化
@@ -38,12 +36,12 @@ public class StringFormat {
 	@Test
 	public void test2(){
 	    Date date=new Date(); // 创建日期对象
-	    System.out.printf("全部日期和时间信息：%tc%n", date);//格式化输出日期或时间
-	    System.out.printf("年-月-日格式：%tF%n", date);
-	    System.out.printf("月/日/年格式：%tD%n", date);
-	    System.out.printf("HH:MM:SS PM格式(12时制)：%tr%n", date);
-	    System.out.printf("HH:MM:SS格式(24时制)：%tT%n", date);
-	    System.out.printf("HH:MM格式(24时制)：%tR%n", date);
+	    System.out.println(String.format("全部日期和时间信息：%tc%n", date));//格式化输出日期或时间
+	    System.out.println(String.format("年-月-日格式：%tF%n", date));
+	    System.out.println(String.format("月/日/年格式：%tD%n", date));
+	    System.out.println(String.format("HH:MM:SS PM格式(12时制)：%tr%n", date));
+	    System.out.println(String.format("HH:MM:SS格式(24时制)：%tT%n", date));
+	    System.out.println(String.format("HH:MM格式(24时制)：%tR%n", date));
 	}
 	/*
 	3、格式化日期字符串
@@ -74,21 +72,18 @@ public class StringFormat {
 	@Test
 	public void test3(){
 	    Date date=new Date();                                                                             // 创建日期对象
-	    String str=String.format(Locale.US,"英文月份简称：%tb",date);      // 格式化日期字符串
-	    System.out.println(str);                                                                              // 输出字符串内容
-	    System.out.printf("本地月份简称：%tb%n",date);
-	    str=String.format(Locale.US,"英文月份全称：%tB",date);
-	    System.out.println(str);
-	    System.out.printf("本地月份全称：%tB%n",date);
-	    str=String.format(Locale.US,"英文星期的简称：%ta",date);
-	    System.out.println(str);
-	    System.out.printf("本地星期的简称：%tA%n",date);
-	    System.out.printf("年的前两位数字(不足两位前面补0)：%tC%n",date);
-	    System.out.printf("年的后两位数字(不足两位前面补0)：%ty%n",date);
-	    System.out.printf("一年中的天数(即年的第几天)：%tj%n",date);
-	    System.out.printf("两位数字的月份(不足两位前面补0)：%tm%n",date);
-	    System.out.printf("两位数字的日(不足两位前面补0)：%td%n",date);
-	    System.out.printf("月份的日(前面不补0)：%te",date);
+	    System.out.println(String.format(Locale.US,"英文月份简称：%tb",date));                                                                              // 输出字符串内容
+	    System.out.println(String.format("本地月份简称：%tb%n",date));
+	    System.out.println(String.format(Locale.US,"英文月份全称：%tB",date));
+	    System.out.println(String.format("本地月份全称：%tB%n",date));
+	    System.out.println(String.format(Locale.US,"英文星期的简称：%ta",date));
+	    System.out.println(String.format("本地星期的简称：%tA%n",date));
+	    System.out.println(String.format("年的前两位数字(不足两位前面补0)：%tC%n",date));
+	    System.out.println(String.format("年的后两位数字(不足两位前面补0)：%ty%n",date));
+	    System.out.println(String.format("一年中的天数(即年的第几天)：%tj%n",date));
+	    System.out.println(String.format("两位数字的月份(不足两位前面补0)：%tm%n",date));
+	    System.out.println(String.format("两位数字的日(不足两位前面补0)：%td%n",date));
+	    System.out.println(String.format("月份的日(前面不补0)：%te",date));
 	}
 	/*
 	4、格式化时间字符串
@@ -113,20 +108,19 @@ public class StringFormat {
 	@Test
 	public void test4(){
 	    Date date=new Date();                               // 创建日期对象
-	    System.out.printf("2位数字24时制的小时(不足2位前面补0):%tH%n",date);
-	    System.out.printf("2位数字12时制的小时(不足2位前面补0):%tI%n",date);
-	    System.out.printf("2位数字24时制的小时(前面不补0):%tk%n",date);
-	    System.out.printf("2位数字12时制的小时(前面不补0):%tl%n",date);
-	    System.out.printf("2位数字的分钟(不足2位前面补0):%tM%n",date);
-	    System.out.printf("2位数字的秒(不足2位前面补0):%tS%n",date);
-	    System.out.printf("3位数字的毫秒(不足3位前面补0):%tL%n",date);
-	    System.out.printf("9位数字的毫秒数(不足9位前面补0):%tN%n",date);
-	    String str=String.format(Locale.US,"小写字母的上午或下午标记(英)：%tp",date);
-	    System.out.println(str);                          // 输出字符串变量str的内容
-	    System.out.printf ("小写字母的上午或下午标记(中)：%tp%n",date);
-	    System.out.printf("相对于GMT的RFC822时区的偏移量:%tz%n",date);
-	    System.out.printf("时区缩写字符串:%tZ%n",date);
-	    System.out.printf("1970-1-1 00:00:00 到现在所经过的秒数：%ts%n",date);
-	    System.out.printf("1970-1-1 00:00:00 到现在所经过的毫秒数：%tQ%n",date);
+	    System.out.println(String.format("2位数字24时制的小时(不足2位前面补0):%tH%n",date));
+	    System.out.println(String.format("2位数字12时制的小时(不足2位前面补0):%tI%n",date));
+	    System.out.println(String.format("2位数字24时制的小时(前面不补0):%tk%n",date));
+	    System.out.println(String.format("2位数字12时制的小时(前面不补0):%tl%n",date));
+	    System.out.println(String.format("2位数字的分钟(不足2位前面补0):%tM%n",date));
+	    System.out.println(String.format("2位数字的秒(不足2位前面补0):%tS%n",date));
+	    System.out.println(String.format("3位数字的毫秒(不足3位前面补0):%tL%n",date));
+	    System.out.println(String.format("9位数字的毫秒数(不足9位前面补0):%tN%n",date));
+	    System.out.println(String.format(Locale.US,"小写字母的上午或下午标记(英)：%tp",date));                          // 输出字符串变量str的内容
+	    System.out.println (String.format("小写字母的上午或下午标记(中)：%tp%n",date));
+	    System.out.println(String.format("相对于GMT的RFC822时区的偏移量:%tz%n",date));
+	    System.out.println(String.format("时区缩写字符串:%tZ%n",date));
+	    System.out.println(String.format("1970-1-1 00:00:00 到现在所经过的秒数：%ts%n",date));
+	    System.out.println(String.format("1970-1-1 00:00:00 到现在所经过的毫秒数：%tQ%n",date));
 	}
 }
