787ea8f4e424d1c25ef960992b026e44280d267a
love-somnus <928200207@qq.com>
2015-03-28 00:25:05 +0800

    version1.1.1

--- src/main/java/com/somnus/thread/ThreadTest11.java
+++ src/main/java/com/somnus/thread/ThreadTest11.java
@@ -1,33 +1,35 @@
 package com.somnus.thread;
-
-public class ThreadTest11
-{
-	public static void main(String[] args)
-	{
+/**
+ * Java中的每个对象都有一个锁（lock）或者叫做监视器（monitor），当访问某个对象的synchronized方法时，
+ * 表示将该对象上锁，此时其它任何线程都无法再去访问该synchronized方法了，直到之前的那个线程执行方法完毕后（或者抛出了异常）
+ * 那么将该对象的锁释放掉，其它线程才有可能再去访问该synchronized方法
+ * 
+ * 如果一个对象有多个synchronized方法，某一时刻某个线程已经进入到了某个synchronized方法，
+ * 那么该方法没有执行完毕前，其它线程是无法访问该对象的任何synchronized方法的
+ * 
+ */
+public class ThreadTest11{
+	public static void main(String[] args){
 		final Outputter output = new Outputter();
 
-		new Thread()
-		{
+		new Thread(){
 			public void run()
 			{
-				output.output("zhangsan");
+				output.output("abcdefghijklmnopqrstuvwxyz");
 			};
 		}.start();
 
-		new Thread()
-		{
+		new Thread(){
 			public void run()
 			{
-				output.output("lisi");
+				output.output("0123456789");
 			};
 		}.start();
 	}
 }
 
-class Outputter
-{
-	public void output(String name)
-	{
+class Outputter{
+	public void output(String name){
 		// TODO 为了保证对name的输出不是一个原子操作，这里逐个输出name的每个字符
 		for (int i = 0; i < name.length(); i++)
 		{
@@ -35,8 +37,7 @@ class Outputter
 		}
 	}
 
-	public void output2(String name)
-	{
+	public void output2(String name){
 		synchronized (this)
 		{
 			for (int i = 0; i < name.length(); i++)
@@ -46,8 +47,7 @@ class Outputter
 		}
 	}
 	
-	public synchronized void output3(String name)
-	{
+	public synchronized void output3(String name){
 		// TODO 为了保证对name的输出不是一个原子操作，这里逐个输出name的每个字符
 		for (int i = 0; i < name.length(); i++)
 		{
--- src/main/java/com/somnus/thread/ThreadTest15.java
+++ src/main/java/com/somnus/thread/ThreadTest15.java
@@ -1,11 +1,17 @@
 package com.somnus.thread;
-
-public class ThreadTest15
-{
-	public static void main(String[] args)
-	{
+/**
+ * wait与notify方法都是定义在Object类中，而且是final的，因此会被所有的Java类所继承并且无法重写。
+ * 这两个方法要求在调用时线程应该已经获得了对象的锁，因此对这两个方法的调用需要放在synchronized方法或块当中。
+ * 当线程执行了wait方法时，它会释放掉对象的锁。
+ * 
+ * 另一个会导致线程暂停的方法就是Thread类的sleep方法，它会导致线程睡眠指定的毫秒数，
+ * 但线程在睡眠的过程中是不会释放掉对象的锁的。
+ *
+ */
+public class ThreadTest15{
+	public static void main(String[] args){
 		final Math math = new Math();
-		for(int i=0;i<2;i++)
+		for(int i=0;i<3;i++)
 		{
 			new Thread(new Runnable(){
 				public void run()
@@ -30,13 +36,11 @@ public class ThreadTest15
 	}
 }
 
-class Math
-{
+class Math{
 	private int num;
 
-	public synchronized void increase()
-	{
-		if(num != 0)
+	public synchronized void increase(){
+		if(num != 0)//while(num != 0)
 		{
 			try
 			{
@@ -51,9 +55,8 @@ class Math
 		System.out.println(Thread.currentThread().getName() + "[" + num+"]");
 		notify();
 	}
-	public synchronized void decrease()
-	{
-		if(num == 0)//如果用if此线程唤醒后，讲继续往下执行
+	public synchronized void decrease(){
+		if(num == 0)//while(num == 0)
 		{
 			try
 			{
