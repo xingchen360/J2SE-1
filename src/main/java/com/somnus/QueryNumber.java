043fa95c6d423526c903023f09c4c1fcc65dc5fd
love-somnus <928200207@qq.com>
2015-03-30 14:28:51 +0800

    version1.1.1

--- src/main/java/com/somnus/QueryNumber.java
+++ src/main/java/com/somnus/QueryNumber.java
@@ -1,55 +1,59 @@
 package com.somnus;
 
-public class QueryNumber
-{
+public class QueryNumber{
 	/*
 	 * 5*4*3*2*1
 	 */
-	public int multiplication(int num)
-	{
-		if (1 == num)
-		{
+	public int multiplication(int num){
+		if (1 == num){
 			return 1;
 		}
-		else
-		{
+		else{
 			return num * multiplication(num - 1);
 		}
 	}
 	/*
 	 * 1+2+3+4+5+6+7+8+9+10
 	 */
-	public int sum(int num)
-	{
-		if (1 == num)
-		{
+	public int sum(int num){
+		if (1 == num){
 			return 1;
 		}
-		else
-		{
+		else{
 			return num + sum(num - 1);
 		}
 	}
 	/*
 	 * 1,1,2,3,5,8,13,21....
 	 */
-	public int compute(int n)
-	{
-		if (1 == n || 2 == n)
-		{
+	public int compute(int n){
+		if (1 == n || 2 == n){
 			return 1;
 		}
-		else
-		{
+		else{
 			return compute(n - 1) + compute(n - 2);
 		}
 	}
+	/*
+     * 1,1,2,3,5,8,13,21....
+     */
+    public int compute2(int n){
+        int num1 = 1;
+        int num2 = 1;
+        for(int i=1;i<=n;i++){
+            if (1 != i && 2 != i){
+                num2 = num1 + num2;
+                num1 = num2 - num1;
+            }
+        }
+        return num2;
+    }
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args){
 		QueryNumber query = new QueryNumber();
 		System.out.println(query.sum(100));
 		System.out.println(query.multiplication(5));
 		System.out.println(query.compute(9));
+		System.out.println(query.compute2(9));
 	}
 }
