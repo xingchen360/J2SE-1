97888a58d4289a8dae4ea812cf8a1cbd91b9d75b
love-somnus <928200207@qq.com>
2015-04-02 09:11:32 +0800

    version1.1.1

--- src/main/java/com/somnus/apache/CommonsConfiguration.java
+++ src/main/java/com/somnus/apache/CommonsConfiguration.java
@@ -1,18 +1,30 @@
 package com.somnus.apache;
 
-/** 
- * @description: TODO 
- * @author Somnus
- * date 2015年4月1日 下午6:18:16  
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+/**
+ * @description: TODO
+ * @author Somnus date 2015年4月1日 下午6:18:16
  */
 public class CommonsConfiguration {
+    public void configuration() {
+        try {
+            String url = getClass().getClassLoader().getResource("info.properties").getPath();
+            System.out.println(url);
+            PropertiesConfiguration config = new PropertiesConfiguration(url);
+            config.setProperty("age", "25");
+            config.save();
+
+            Integer integer = config.getInt("age");
+            System.out.println(integer);
+        } catch (ConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        PropertiesConfiguration config = new PropertiesConfiguration("src/main/resources/info.properties");  
-        config.setProperty("age", "21");  
-        config.save();  
-          
-        Integer integer = config.getInteger("age");
-        
-        System.out.println(integer);
+        CommonsConfiguration config = new CommonsConfiguration();
+        config.configuration();
     }
 }
