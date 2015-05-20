d02ed0aad03569967c4a16231cc0a011d4d083d8
love-somnus <928200207@qq.com>
2015-05-20 13:31:15 +0800

    version1.1.1

--- src/main/java/com/somnus/enums/Test.java
+++ src/main/java/com/somnus/enums/Test.java
@@ -5,18 +5,18 @@ public class Test {
 		//枚举是一种类型，用于定义变量，以限制变量的赋值；赋值时通过“枚举名.值”取得枚举中的值
         ColorEnum colorEnum = ColorEnum.blue;
         switch (colorEnum) {
-        case red:
-            System.out.println("color is red");
-            break;
-        case green:
-            System.out.println("color is green");
-            break;
-        case yellow:
-            System.out.println("color is yellow");
-            break;
-        case blue:
-            System.out.println("color is blue");
-            break;
+            case red:
+                System.out.println("color is red");
+                break;
+            case green:
+                System.out.println("color is green");
+                break;
+            case yellow:
+                System.out.println("color is yellow");
+                break;
+            case blue:
+                System.out.println("color is blue");
+                break;
         }
         
         //遍历枚举
@@ -26,35 +26,31 @@ public class Test {
         }
         
         //获取枚举的个数
-        System.out.println("ColorEnum枚举中的值有"+ColorEnum.values().length+"个");
+        System.out.println("ColorEnum枚举中的值有:"+ColorEnum.values().length+"个");
         
         //获取枚举的索引位置，默认从0开始
-        System.out.println(ColorEnum.red.ordinal());//0
-        System.out.println(ColorEnum.green.ordinal());//1
-        System.out.println(ColorEnum.yellow.ordinal());//2
-        System.out.println(ColorEnum.blue.ordinal());//3
+        System.out.println("索引位置："+ColorEnum.red.ordinal());//0
+        System.out.println("索引位置："+ColorEnum.green.ordinal());//1
+        System.out.println("索引位置："+ColorEnum.yellow.ordinal());//2
+        System.out.println("索引位置："+ColorEnum.blue.ordinal());//3
         
         //枚举默认实现了java.lang.Comparable接口
-        System.out.println(ColorEnum.red.compareTo(ColorEnum.green));//-1
+        System.out.println("类型比较："+ColorEnum.red.compareTo(ColorEnum.green));//-1
         
-        //************************************************************************************************************
-        System.out.println("===========");
-        System.out.println("季节为" + SeasonEnum.getSeason());
+        System.out.println("/************************************************************************************************************/");
+        System.out.println("季节为：" + SeasonEnum.getSeason());
         
-      //************************************************************************************************************
-        System.out.println("===========");
+        System.out.println("/************************************************************************************************************/");
         for(GenderEnum gender : GenderEnum.values()){
-            System.out.println(gender.value);
+            System.out.println("迭代得到的值："+gender.value);
         }
-        String value = GenderEnum.MAN.getValue();
-        GenderEnum genum = GenderEnum.getByCode(value);
         
+        GenderEnum genum = GenderEnum.getByCode("0");
         if(genum.getValue().equals(GenderEnum.MAN.getValue())){
             System.out.println("true");
         }
        
-      //************************************************************************************************************
-        System.out.println("===========");
+        System.out.println("/************************************************************************************************************/");
         for(OrderStateEnum order : OrderStateEnum.values()){
             System.out.println(order.getName());
         }
