a9846aab8719e3475cae7c8048426aeedd0b8a67
love-somnus <928200207@qq.com>
2015-05-28 10:50:52 +0800

    version1.1.1

--- src/main/java/com/somnus/io/TestByteArrayInputStream.java
+++ src/main/java/com/somnus/io/TestByteArrayInputStream.java
@@ -3,6 +3,7 @@ package com.somnus.io;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 
 public class TestByteArrayInputStream{
 
@@ -15,25 +16,28 @@ public class TestByteArrayInputStream{
             /**一次性全部读出来*/
             /*byte[] b = new byte[is.available()];
             int len = is.read(b,0,b.length);
+            System.out.println(Arrays.toString(b));
             System.out.println("读入长度为："+len);
             System.out.println(new String(b));*/
             
+            /**一个个字节读*/
             int data = 0;
-            /*
-             * public synchronized int read() {
-             *   return (pos < count) ? (buf[pos++] & 0xff) : -1;
-             * }
-             */
+            byte[] buf = new byte[is.available()];
+            int count = 0;
             while ((data = is.read()) != -1){
+                buf[count++] = (byte)data;
                 System.out.println(data +"|"+(byte)data+"|"
                         +Integer.toBinaryString(data)+"|"+Integer.toHexString(data)+ " ");
             }
+            System.out.println(Arrays.toString(buf));
+            System.out.println(new String(buf));
             
             /**或者分批读*/
-            /*byte[] buff = new byte[128];
+            /*byte[] buf = new byte[128];
             int len = 0;
-            while((len = is.read(buff))!=-1){
-                System.out.print(new String(buff,0,len));
+            while((len = is.read(buf))!=-1){
+                System.out.println(Arrays.toString(buf));
+                System.out.print(new String(buf,0,len));
                 System.out.print("[读取到的长度："+len+"]");
             }*/
         } catch (IOException e) {
