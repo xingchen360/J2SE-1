20861daf27a983b6e9cb1a3bf754a5b74fb42ab8
love-somnus <928200207@qq.com>
2015-06-27 18:34:07 +0800

    version1.1.1

--- src/main/java/com/somnus/proxy/cglib/CglibProxy.java
+++ src/main/java/com/somnus/proxy/cglib/CglibProxy.java
@@ -12,21 +12,13 @@ import net.sf.cglib.proxy.MethodProxy;
  * @author Somnus
  * @date 2015年3月9日 上午9:08:26
  */
-public class CglibProxy implements MethodInterceptor, Serializable {
+public class CglibProxy implements Serializable {
 	private static final long serialVersionUID = 1L;
-
-	private void doBefore() {
-		System.out.println("before method invoke");
-	}
-
-	private void doAfter() {
-		System.out.println("after method invoke");
-	}
 
 	public Object getProxy(Object target) {
 		Enhancer enhancer = new Enhancer();
 		enhancer.setSuperclass(target.getClass());
-		enhancer.setCallback(this);
+		enhancer.setCallback(new Handler());
 		enhancer.setClassLoader(target.getClass().getClassLoader());
 		return enhancer.create();
 	}
@@ -34,23 +26,33 @@ public class CglibProxy implements MethodInterceptor, Serializable {
 	public Object getProxy(Class<? extends Object> clazz){
 		Enhancer enhancer = new Enhancer();
 		enhancer.setSuperclass(clazz);
-        enhancer.setCallback(this);
+        enhancer.setCallback(new Handler());
         enhancer.setInterfaces(new Class[] { Serializable.class });
         return enhancer.create();
 	}
 	
+	public static class Handler implements MethodInterceptor{
+	    private void doBefore() {
+	        System.out.println("before method invoke");
+	    }
 
-	public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
-		try {
-			// 调用之前
-			doBefore();
-			// 调用原始对象的方法
-			Object result = proxy.invokeSuper(obj, args);
-			// 调用之后
-			doAfter();
-			return result;
-		} catch (Throwable e) {
-			throw e;
-		}
+	    private void doAfter() {
+	        System.out.println("after method invoke");
+	    }
+	    @Override
+	    public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+	        try {
+	            // 调用之前
+	            doBefore();
+	            // 调用原始对象的方法
+	            Object result = proxy.invokeSuper(obj, args);
+	            // 调用之后
+	            doAfter();
+	            return result;
+	        } catch (Throwable e) {
+	            throw e;
+	        }
+	    }
 	}
+	
 }
