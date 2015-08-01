43a4f6784c4ed9af26ae434d60f52b8cf9157485
love-somnus <928200207@qq.com>
2015-08-01 17:51:32 +0800

    version1.1.1

--- src/main/java/com/somnus/accessModifier/StaticTest.java
+++ src/main/java/com/somnus/accessModifier/StaticTest.java
@@ -11,7 +11,7 @@ package com.somnus.accessModifier;
  */
 public class StaticTest {
 	public static void main(String[] args){
-		Animal a = new Dog();
+		new Dog();
 	}
 }
 class Animal{
@@ -23,6 +23,7 @@ class Animal{
 	}
 }
 class Dog extends Animal{
+	Cat cat = new Cat();
 	static{
 		System.out.println("Dog static");
 	}
@@ -30,3 +31,19 @@ class Dog extends Animal{
 		System.out.println("Dog constructor");
 	}
 }
+class Cat{
+	public Cat(){
+		System.out.println("Cat constructor");
+	}
+}
+/* 1、在new B一个实例时首先要进行类的装载。（类只有在使用New调用创建的时候才会被java类装载器装入）
+ * 2、在装载类时，先装载父类A，再装载子类B
+ * 3、装载父类A后，完成静态动作（包括静态代码和变量，它们的级别是相同的，安装代码中出现的顺序初始化）
+ * 4、装载子类B后，完成静态动作
+ * 类装载完成，开始进行实例化
+ * 1、在实例化子类B时，先要实例化父类A
+ * 2、实例化父类A时，先成员实例化（非静态代码）
+ * 3、父类A的构造方法
+ * 4、子类B的成员实例化（非静态代码）
+ * 5、子类B的构造方法
+ */
