cc4f621e7745d03c8ff7e2bd9077b58e4e02d0d9
love-somnus <928200207@qq.com>
2015-08-13 16:52:55 +0800

    version1.1.1

--- src/main/java/com/somnus/Grammar.java
+++ src/main/java/com/somnus/Grammar.java
@@ -1,5 +1,7 @@
 package com.somnus;
 
+import org.junit.Test;
+
 /**
  * 
  * @Title: Grammar.java 
@@ -21,19 +23,16 @@ public class Grammar {
 		//精度double 大于 float ，所以不能double向下转型 float ，但float可以向上转型double
 		System.out.println(num1 == num2);
 		System.out.println(num1+"|"+num2+"|"+f1);
-		testBreak();
-		System.out.println("-----------------------");
-		testContinue();
-		System.out.println("-----------------------");
-		testReturn();
 	}
+	
 	/**
 	 * 只能在循环体内和switch语句体内使用break语句。
 	 * 当break出现在循环体中的switch语句体内时，其作用只是跳出该switch语句体。
 	 * 当break出现在循环体中，但并不在switch语句体内时，则在执行break后，跳出本层循环体。
 	 * 在循环结构中，应用break语句使流程跳出本层循环体，从而提前结束本层循环
 	 */
-	static void testBreak(){
+	@Test
+	public void testBreak(){
 		for(int i=0;i<10;i++){
 			if(i%2==0){
 				System.out.println("i="+i);
@@ -44,12 +43,14 @@ public class Grammar {
 			}
 		}
 	}
+	
 	/**
 	 * 其作用是结束本次循环，即跳过本次循环体中余下尚未执行的语句，接着再一次进行循环的条件判定。
 	 * 注意：执行continue语句并没有使整个循环终止。在while和do-while循环中，continue语句使得流程直接跳到循环控制条件的测试部分 ，然后决定循环是否继续进行。
 	 * 在for 循环中，遇到continue后，跳过循环体中余下的语句，而去对for语句中的“表达式3”求值，然后进行“表达式2”的条件测试，  最后根据“表达式2”的值来决定for循环是否执行。
 	 */
-	static void testContinue(){
+	@Test
+	public void testContinue(){
 		for(int i=0;i<10;i++){
 			if(i%2==0){
 				System.out.println("没有执行continue语句输出i="+i);
@@ -62,8 +63,8 @@ public class Grammar {
 		}
 	}
 
-	@SuppressWarnings("unused")
-	static void testReturn(){
+	@Test
+	public void testReturn(){
 		for(int i=0;i<10;i++){
 			System.out.println("执行了return语句,直接跳出了当前的testReturn方法!");
 			return;
