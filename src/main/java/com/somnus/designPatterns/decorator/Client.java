9a24a42dcdbe2c9ac1de7d74f4419b9f6763a419
love-somnus <928200207@qq.com>
2015-08-18 18:18:20 +0800

    version1.1.1

--- src/main/java/com/somnus/designPatterns/decorator/Client.java
+++ src/main/java/com/somnus/designPatterns/decorator/Client.java
@@ -5,7 +5,7 @@ public class Client{
 	public static void main(String[] args){
 	    Component component,componentSB;  //使用抽象构件定义
         component = new Window(); //定义具体构件
-        componentSB = new  ScrollBarDecorator(component); //定义装饰后的构件
+        componentSB = new ScrollBarDecorator(component); //定义装饰后的构件
         componentSB.display();
 	}
 	
