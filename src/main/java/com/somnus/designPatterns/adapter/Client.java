a1b48fe1dea46e9d7c07756d27fbf40fad13141f
love-somnus <928200207@qq.com>
2015-08-26 17:16:44 +0800

    version1.1.1

--- src/main/java/com/somnus/designPatterns/adapter/Client.java
+++ src/main/java/com/somnus/designPatterns/adapter/Client.java
@@ -4,12 +4,10 @@ public class Client {
     
     public static void main(String[] args) throws Exception {
         ScoreOperation operation = (ScoreOperation)XMLUtil.getBean(); //读取配置文件，反射生成对象  
-        int scores[] = {84,76,50,69,90,91,88,96}; //定义成绩数组  
-        int result[];  
-        int score;  
+        int[] scores = {84,76,50,69,90,91,88,96}; //定义成绩数组  
           
         System.out.println("成绩排序结果：");  
-        result = operation.sort(scores);  
+        int[] result = operation.sort(scores);  
   
         //遍历输出成绩  
         for(int i : scores) {  
@@ -17,8 +15,8 @@ public class Client {
         }  
         System.out.println();  
           
-        System.out.println("查找成绩90：");  
-        score = operation.search(result,90);  
+        System.out.print("查找成绩90：");  
+        int score = operation.search(result,90);  
         if (score != -1) {  
             System.out.println("找到成绩90。");  
         }  
@@ -26,7 +24,7 @@ public class Client {
             System.out.println("没有找到成绩90。");  
         }  
           
-        System.out.println("查找成绩92：");  
+        System.out.print("查找成绩92：");  
         score = operation.search(result,92);  
         if (score != -1) {  
             System.out.println("找到成绩92。");  
