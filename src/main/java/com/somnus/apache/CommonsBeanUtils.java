6c98542d4a5cbbd10550072be235349f83db2889
love-somnus <928200207@qq.com>
2015-10-19 21:11:23 +0800

    version1.1.1

--- src/main/java/com/somnus/apache/CommonsBeanUtils.java
+++ src/main/java/com/somnus/apache/CommonsBeanUtils.java
@@ -1,9 +1,12 @@
 package com.somnus.apache;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
+
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.beanutils.PropertyUtils;
+import org.junit.Test;
 
 /**
  * 
@@ -16,39 +19,53 @@ import org.apache.commons.beanutils.PropertyUtils;
  */
 public class CommonsBeanUtils {
     
-	public static void main(String[] args) throws Exception {
-		// ****************************************************************************
-		Person person = new Person();
-		person.setName("tom");
-		person.setAge(21);
-		/*克隆对象*/
-		Person person2 = (Person) BeanUtils.cloneBean(person);
-		System.out.println("克隆对象>>" + person2);
-		// ****************************************************************************
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("name", "tom");
-		map.put("email", "tom@");
-		map.put("age", "21");
-		/*将map转化为一个Person对象*/
-		Person person3 = new Person();
-		BeanUtils.populate(person3, map);
-		System.out.println("将map转化为一个Person对象>>" + person3);
-		/*通过上面的一行代码，此时person的属性就已经具有了上面所赋的值了。*/
-		/*将一个Bean转化为一个Map对象了，如下：*/
-		Map<String, String> map2 = BeanUtils.describe(person3);
-		System.out.println(map2.get("name") + ">>" + map2.get("age"));
-		/*拥有相同属性的对象转换*/
-		People people = new People();
-		BeanUtils.copyProperties(people, person);
-		System.out.println("拥有相同属性的对象转换>>" + people);
-		// ****************************************************************************
-		Person person4 = new Person();
-		person4.setName("andy");
-		/*反射调用get方法*/
-		String name = (String) PropertyUtils.getProperty(person4, "name");
-		System.out.println(name);
-		/*反射调用set方法*/
-		PropertyUtils.setProperty(person4, "age", 25);
-		System.out.println(person4.getAge());
-	}
+    @Test
+    public void cloneBean() throws IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException{
+        Person person = new Person();
+        person.setName("tom");
+        person.setAge(21);
+        /*克隆对象*/
+        Person person2 = (Person) BeanUtils.cloneBean(person);
+        System.out.println("克隆对象>>" + person2);
+    }
+    
+    @Test
+    public void populate() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException{
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("name", "tom");
+        map.put("email", "tom@");
+        map.put("age", "21");
+        /*将map转化为一个Person对象*/
+        Person person = new Person();
+        BeanUtils.populate(person, map);
+        System.out.println("将map转化为一个Person对象>>" + person);
+        /*通过上面的一行代码，此时person的属性就已经具有了上面所赋的值了。*/
+        /*将一个Bean转化为一个Map对象了，如下：*/
+        Map<String, String> map2 = BeanUtils.describe(person);
+        System.out.println(map2.get("name") + ">>" + map2.get("age"));
+    }
+    
+    @Test
+    public void copyProperties() throws IllegalAccessException, InvocationTargetException{
+        Person person = new Person();
+        person.setName("tom");
+        person.setAge(21);
+        /*拥有相同属性的对象转换*/
+        People people = new People();
+        BeanUtils.copyProperties(people, person);
+        System.out.println("拥有相同属性的对象转换>>" + people);
+    }
+    
+    @Test
+    public void reflect() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException{
+        Person person = new Person();
+        person.setName("andy");
+        /*反射调用get方法*/
+        String name = (String) PropertyUtils.getProperty(person, "name");
+        System.out.println(name);
+        /*反射调用set方法*/
+        PropertyUtils.setProperty(person, "age", 25);
+        System.out.println(person.getAge());
+    }
+    
 }
--- src/main/java/com/somnus/apache/CommonsLang.java
+++ src/main/java/com/somnus/apache/CommonsLang.java
@@ -1,6 +1,7 @@
 package com.somnus.apache;
 
 import java.io.File;
+import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -12,6 +13,7 @@ import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateFormatUtils;
 import org.apache.commons.lang3.time.DateUtils;
+import org.junit.Test;
 
 /**
  * 
@@ -24,80 +26,106 @@ import org.apache.commons.lang3.time.DateUtils;
  */
 public class CommonsLang {
     
-	public static void main(String[] args) throws Exception {
-		// 将两个数组合并为一个数组
-		String[] s1 = new String[] { "1", "2", "3" };
-		String[] s2 = new String[] { "a", "b", "c" };
-		String[] s = (String[]) ArrayUtils.addAll(s1, s2);
-		for (int i = 0; i < s.length; i++) {
-			System.out.println(s[i]);
-		}
-		String str = ArrayUtils.toString(s);
-		str = str.substring(1, str.length() - 1);
-		System.out.println(str + ">>" + str.length());
-		System.out.println("********************************ArrayUtils***************************************");
-		// 取得类名
-		System.out.println(ClassUtils.getShortClassName(CommonsLang.class));
-		// 取得其包名
-		System.out.println(ClassUtils.getPackageName(CommonsLang.class));
-		// 五位的随机字母和数字 
-		System.out.println(RandomStringUtils.randomAlphanumeric(5));
-		System.out.println("********************************ClassUtils***************************************");
-		System.out.println(StringEscapeUtils.escapeHtml3("<html>"));
-		// 输出结果为&lt;html&gt;
-		System.out.println(StringEscapeUtils.escapeJava("String"));
-		System.out.println("********************************StringEscapeUtils***************************************");
-		// 截取从from开始字符串
-		System.out.println(StringUtils.substringAfter("SELECT * FROM PERSON " , "FROM"));
-		// 判断该字符串是不是为数字(0~9)组成，如果是，返回true 但该方法不识别有小数点和请注意
-		System.out.println(StringUtils.isNumeric("454534"));// 返回true
-		// StringUtils,判断是否是空格字符
-		/**
-		 * StringUtils.isBlank(null)      = true
-		 * StringUtils.isBlank("")        = true
-		 * StringUtils.isBlank(" ")       = true
-		 * StringUtils.isBlank("bob")     = false
-		 * StringUtils.isBlank("  bob  ") = false
-		 */
-		System.out.println(StringUtils.isBlank("   "));
-		/**
-		 * StringUtils.isEmpty(null)      = true
-		 * StringUtils.isEmpty("")        = true
-		 * StringUtils.isEmpty(" ")       = false
-		 * StringUtils.isEmpty("bob")     = false
-		 * StringUtils.isEmpty("  bob  ") = false
-		 */
-		System.out.println(StringUtils.isEmpty("   "));
-		// 将数组中的内容以,分隔
-		System.out.println(StringUtils.join(new String[] { "1", "2", "3" }, ","));
-		// 在右边加下字符,使之总长度为6
-		System.out.println(StringUtils.rightPad("abc", 6, 'T'));
-		// 首字母大写 
-		System.out.println(StringUtils.capitalize("abc"));
-		// Deletes all whitespaces from a String 删除所有空格
-		System.out.println(StringUtils.deleteWhitespace("   ab  c  "));
-		// 判断是否包含这个字符 
-		System.out.println(StringUtils.contains("abc", "ba"));
-		// 表示左边两个字符 
-		System.out.println(StringUtils.left("abc", 2));
-		System.out.println("********************************StringUtils***************************************");
-		System.out.println(DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss"));
-		//直接将日期格式化为内置的固定格式
-		System.out.println(DateFormatUtils.ISO_DATE_FORMAT.format(new Date()));
-		//字符型日期转化为Date
-		System.out.println(DateUtils.parseDate("2014-11-11 11:11:11", new String[] { "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm", "yyyy-MM-dd", "yyyy/MM/dd" }));
-		//日期舍入与截整
-		System.out.println(DateUtils.truncate(new Date(), Calendar.DATE));
-		//判断是否是同一天
-		System.out.println(DateUtils.isSameDay(new Date(), new Date()));
-		//加天数
-		System.out.println(DateUtils.addDays(new Date(), 10));
-		System.out.println("********************************DateUtils***************************************");
-		//validate
-		File sourceFile = new File(CommonsLang.class.getClassLoader().getResource("").getPath());
+    @Test
+    public void arrayUtils(){
+        // 将两个数组合并为一个数组
+        String[] s1 = new String[] { "1", "2", "3" };
+        String[] s2 = new String[] { "a", "b", "c" };
+        String[] s = (String[]) ArrayUtils.addAll(s1, s2);
+        for (int i = 0; i < s.length; i++) {
+            System.out.println(s[i]);
+        }
+        String str = ArrayUtils.toString(s);
+        str = str.substring(1, str.length() - 1);
+        System.out.println(str + ">>" + str.length());
+    }
+    
+    @Test
+    public void ClassUtils(){
+        // 取得类名
+        System.out.println(ClassUtils.getShortClassName(CommonsLang.class));
+        // 取得其包名
+        System.out.println(ClassUtils.getPackageName(CommonsLang.class));
+        // 五位的随机字母和数字 
+        System.out.println(RandomStringUtils.randomAlphanumeric(5));
+    }
+    
+    @Test
+    public void StringEscapeUtils(){
+        System.out.println(StringEscapeUtils.escapeHtml3("<html>"));
+        // 输出结果为&lt;html&gt;
+        System.out.println(StringEscapeUtils.escapeJava("String"));
+    }
+    
+    @Test
+    public void stringUtils(){
+        // 截取从from开始字符串
+        System.out.println(StringUtils.substringAfter("SELECT * FROM PERSON " , "FROM"));
+        // 判断该字符串是不是为数字(0~9)组成，如果是，返回true 但该方法不识别有小数点和请注意
+        System.out.println(StringUtils.isNumeric("454534"));// 返回true
+        // StringUtils,判断是否是空格字符
+        /**
+         * StringUtils.isBlank(null)      = true
+         * StringUtils.isBlank("")        = true
+         * StringUtils.isBlank(" ")       = true
+         * StringUtils.isBlank("bob")     = false
+         * StringUtils.isBlank("  bob  ") = false
+         */
+        System.out.println(StringUtils.isBlank("   "));
+        /**
+         * StringUtils.isEmpty(null)      = true
+         * StringUtils.isEmpty("")        = true
+         * StringUtils.isEmpty(" ")       = false
+         * StringUtils.isEmpty("bob")     = false
+         * StringUtils.isEmpty("  bob  ") = false
+         */
+        System.out.println(StringUtils.isEmpty("   "));
+        // 将数组中的内容以,分隔
+        System.out.println(StringUtils.join(new String[] { "1", "2", "3" }, ","));
+        // 在右边加下字符,使之总长度为6
+        System.out.println(StringUtils.rightPad("abc", 6, 'T'));
+        // 首字母大写 
+        System.out.println(StringUtils.capitalize("abc"));
+        // Deletes all whitespaces from a String 删除所有空格
+        System.out.println(StringUtils.deleteWhitespace("   ab  c  "));
+        // 判断是否包含这个字符 
+        System.out.println(StringUtils.contains("abc", "ba"));
+        // 表示左边两个字符 
+        System.out.println(StringUtils.left("abc", 2));
+        /**
+         * StringUtils.remove(null, *)        = null
+         * StringUtils.remove("", *)          = ""
+         * StringUtils.remove(*, null)        = *
+         * StringUtils.remove(*, "")          = *
+         * StringUtils.remove("queued", "ue") = "qd"
+         * StringUtils.remove("queued", "zz") = "queued"
+         */
+        System.out.println(StringUtils.remove("2015-10-01", "-"));
+    }
+    
+    @Test
+    public void DateUtils() throws ParseException{
+        System.out.println(DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss"));
+        //直接将日期格式化为内置的固定格式
+        System.out.println(DateFormatUtils.ISO_DATE_FORMAT.format(new Date()));
+        //字符型日期转化为Date
+        System.out.println(DateUtils.parseDate("2014-11-11 11:11:11", new String[] { "yyyy-MM-dd HH:mm:ss","yyyy-MM-dd", "yyyy/MM/dd" }));
+        System.out.println(DateUtils.parseDate("2014年11月11日", new String[] { "yyyy年MM月dd日","yyyy-MM-dd", "yyyy/MM/dd" }));
+        //日期舍入与截整
+        System.out.println(DateUtils.truncate(new Date(), Calendar.DATE));
+        //判断是否是同一天
+        System.out.println(DateUtils.isSameDay(new Date(), new Date()));
+        //加天数
+        System.out.println(DateUtils.addDays(new Date(), 10));
+    }
+    
+    @Test
+    public void Validate(){
+        File sourceFile = new File(CommonsLang.class.getClassLoader().getResource("").getPath());
         Validate.notNull(sourceFile, "source file is required.");
         Validate.isTrue(sourceFile.isFile(), "source file [%s] is not a valid file.", sourceFile);
         String string = "";
         Validate.notEmpty(string, "string is not empty");
-	}
+    }
+    
 }
