5d2b4b11a308638cbb4e12c0ecf6cdbcf92f7156
love-somnus <928200207@qq.com>
2015-10-31 22:52:54 +0800

    version1.1.1

--- src/main/java/com/somnus/apache/CommonsCollections.java
+++ src/main/java/com/somnus/apache/CommonsCollections.java
@@ -1,6 +1,7 @@
 package com.somnus.apache;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.collections.CollectionUtils;
@@ -18,29 +19,32 @@ public class CommonsCollections {
     
 	public static void main(String[] args) {
 
-		List<String> list0 = new ArrayList<String>();
-		List<String> list1 = new ArrayList<String>();
-		list1.add("1");
-		list1.add("2");
-		list1.add("3");
-		List<String> list2 = new ArrayList<String>();
-		list2.add("2");
-		list2.add("3");
-		list2.add("5");
+		List<String> emptyList = new ArrayList<String>();
 		
 		/*集合判断： 
 		例1: 判断集合是否为空:*/
-		System.out.println(CollectionUtils.isEmpty(list0));
-		System.out.println(CollectionUtils.isEmpty(list1));
+		System.out.println(CollectionUtils.isEmpty(emptyList));
+		System.out.println(CollectionUtils.isEmpty(Arrays.asList("1", "2", "3")));
 
 		/*例2: 判断集合是否不为空:*/
-		System.out.println(CollectionUtils.isNotEmpty(list0));
-		System.out.println(CollectionUtils.isNotEmpty(list1));
+		System.out.println(CollectionUtils.isNotEmpty(emptyList));
+		System.out.println(CollectionUtils.isNotEmpty(Arrays.asList("1", "2", "3")));
 
 		
-		System.out.println(CollectionUtils.union(list1, list2));//并集{3, 2, 1, 5}
-		System.out.println(CollectionUtils.intersection(list1, list2));//交集{3, 2}
-		System.out.println(CollectionUtils.disjunction(list1, list2));//交集的补集{1, 5}
-		System.out.println(CollectionUtils.subtract(list1, list2));//list1与list2的差{1}
+		System.out.println(CollectionUtils.union(
+				Arrays.asList("1", "2", "3"), 
+				Arrays.asList("2", "3", "5")));//并集{3, 2, 1, 5}
+		
+		System.out.println(CollectionUtils.intersection(
+				Arrays.asList("1", "2", "3"), 
+				Arrays.asList("2", "3", "5")));//交集{3, 2}
+		
+		System.out.println(CollectionUtils.disjunction(
+				Arrays.asList("1", "2", "3"), 
+				Arrays.asList("2", "3", "5")));//交集的补集{1, 5}
+		
+		System.out.println(CollectionUtils.subtract(
+				Arrays.asList("2", "3", "5"), 
+				Arrays.asList("1", "2", "3")));//list1与list2的差{1}
 	}
 }
