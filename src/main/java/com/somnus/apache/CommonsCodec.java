24b05b1b04664fa7b4a8ac76e19e4278c48fdfa8
love-somnus <928200207@qq.com>
2015-11-06 22:05:58 +0800

    version1.1.1

--- src/main/java/com/somnus/apache/CommonsCodec.java
+++ src/main/java/com/somnus/apache/CommonsCodec.java
@@ -2,13 +2,13 @@ package com.somnus.apache;
 
 import java.util.Arrays;
 
+import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.junit.Test;
 
 /**
- * 
- * @Title: CommonsCodec.java 
- * @Package com.somnus.apache 
  * @Description: TODO
  * @author Somnus
  * @date 2015年6月12日 下午5:38:47 
@@ -16,17 +16,19 @@ import org.apache.commons.codec.binary.Hex;
  */
 public class CommonsCodec {
 	
-	public static void main(String[] args) throws Exception {
+	@Test
+	public void Base64(){
 		String encode = Base64.encodeBase64String("Somnus罂粟花".getBytes(/*"UTF-8"*/));
 		System.out.println("Base64 编码后：" + encode);
 		
 		byte[] decode = Base64.decodeBase64("U29tbnVz");
 		String decodestr = new String(decode);  
-        System.out.println("Base64 解码后："+decodestr);  
-        
-        System.out.println("*********************************************************");
-        
-        byte[] buff = "Somnus罂粟花".getBytes(/*"utf-8"*/);
+        System.out.println("Base64 解码后："+decodestr); 
+	}
+	
+	@Test
+	public void Hex() throws DecoderException{
+		byte[] buff = "Somnus罂粟花".getBytes(/*"utf-8"*/);
         System.out.println(Arrays.toString(buff));
         
         String byte2hex = Hex.encodeHexString(buff);
@@ -35,5 +37,11 @@ public class CommonsCodec {
         byte[] hex2byte = Hex.decodeHex(byte2hex.toCharArray());
         System.out.println(Arrays.toString(hex2byte));
         System.out.println(new String(hex2byte));
+	}
+	
+	@Test
+	public void DigestUtils(){
+		System.out.println(DigestUtils.md5Hex("admin"));
+		System.out.println(DigestUtils.shaHex("admin"));
 	}
 }
