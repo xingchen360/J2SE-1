a2d2ab3daf49e08b325c5f151a930416c2ea1546
love-somnus <928200207@qq.com>
2015-12-29 13:10:34 +0800

    version1.1.1

---
+++ src/main/java/com/somnus/SystemUtil.java
@@ -0,0 +1,55 @@
+package com.somnus;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+
+/**   
+ * @Title: SystemUtil
+ * @Description: TODO
+ * @author: cjliu
+ * @date: 2012-9-13下午7:30:00
+ * @version: 1.0  
+ */
+public class SystemUtil {
+	private static String ip = null;
+	private static String machine = null;
+
+	private static void init(){
+		InetAddress addr = null;
+	    try {
+			addr = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		 
+		ip=addr.getHostAddress().toString();//获得本机IP 
+		machine=addr.getHostName().toString();//获得本机名称 
+	}
+	/**
+	 * 
+	 * @return 机器IP
+	 */
+	public static String getIp(){
+		if(ip == null){
+			init();
+		}
+		return ip;
+	}
+	/**
+	 * 
+	 * @return 机器编号
+	 */
+	public static String getMachine(){
+		if(machine == null){
+			init();
+		}
+		return machine;
+	}
+	
+	public static void main(String[] args) {
+        System.out.println(getIp());
+        System.out.println(getMachine());
+    }
+	
+}
