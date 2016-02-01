e659a534a0d80bc1632ec8db7f3674d99dab20f5
love-somnus <928200207@qq.com>
2016-02-01 18:13:21 +0800

    version1.1.1

--- src/main/java/com/somnus/accessModifier/StringTest.java
+++ src/main/java/com/somnus/accessModifier/StringTest.java
@@ -1,12 +1,32 @@
 package com.somnus.accessModifier;
 
+import org.junit.Test;
+
 public class StringTest {
 	
-	public static void main(String[] args) {
+    @Test
+	public void strTest() {
 		String str = "abcd";
 		String str2 = "cd";
 		System.out.println(str == "ab" + "cd");
 		System.out.println(str == "ab" + str2);
 	}
 	
+    @Test
+    public void strTest2() {
+        String str = "abcd"; 
+        final String a = "ab";
+        String b = "ab";
+        System.out.println(str == a + "cd");
+        System.out.println(str == b + "cd");
+    }
+    
+    @Test
+    public void strTest3() {
+        String str = "abcd"; 
+        final String a = "cd";
+        String b = "cd";
+        System.out.println(str == "ab" + a);
+        System.out.println(str == "ab" + b);
+    }
 }
--- src/main/java/com/somnus/statics/Test2.java
+++ src/main/java/com/somnus/statics/Test2.java
@@ -2,6 +2,8 @@ package com.somnus.statics;
 
 import java.util.Random;
 
+import org.junit.Test;
+
 class T{
 	public static final int a = 8/2;//不会初始化，【编译的时候就能确定值】
 	public static final int aa = new Random().nextInt(100);
@@ -19,19 +21,29 @@ class T{
 	
 }
 public class Test2{
-	public static void main(String[] args){
-        /*System.out.println(T.a);
-        System.out.println(T.aa);
-        System.out.println(T.b);
+    
+    @Test
+	public void test1(){
+        System.out.println(T.a);
+        /*System.out.println(T.b);
         System.out.println(T.c);*/
-		
-		T t = new T();
-		t.d = 6;
-		T.b = 8;
-		System.out.println(t.d);
-		
-		T t2 = new T();
-		System.out.println(t2.d);
-		System.out.println(t2.b);
 	}
+    @Test
+    public void test2(){
+        System.out.println(T.aa);
+        /*System.out.println(T.b);
+        System.out.println(T.c);*/
+    }
+    
+    @Test
+    public void test3(){
+        T t = new T();
+        t.d = 6;
+        T.b = 8;
+        System.out.println(t.d);
+        
+        T t2 = new T();
+        System.out.println(t2.d);
+        System.out.println(t2.b);
+    }
 }
