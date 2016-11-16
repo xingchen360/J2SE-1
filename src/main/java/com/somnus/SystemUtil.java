768d6b84505b757ce91330d536e4e755c275c2cf
somnus <928200207@qq.com>
2016-11-16 14:31:23 +0800

    aliyun

--- src/main/java/com/somnus/SystemUtil.java
+++ src/main/java/com/somnus/SystemUtil.java
@@ -2,6 +2,8 @@ package com.somnus;
 
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Enumeration;
 
 /**
@@ -12,68 +14,77 @@ import java.util.Enumeration;
  * @version V1.0
  */
 public class SystemUtil {
+	
+	private static String ip = null;
+	private static String hostName = null;
+	
+	private static void init() throws UnknownHostException, SocketException {
+		InetAddress  localhost = null;
+		try {
+			//windows系统
+            localhost = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+        	e.printStackTrace();
+        	//linux系统
+        	localhost = getLocalHost();
+        }
+		ip = localhost.getHostAddress().toString();
+		hostName = localhost.getHostName().toString();
+	}
+	
+	public static String getLocalIp(){
+		if(ip == null){
+			try {
+				init();
+			} catch (UnknownHostException | SocketException e) {
+				e.printStackTrace();
+			}
+		}
+		return ip;
+	}
+	
+	public static String getLocalHostName(){
+		if(hostName == null){
+			try {
+				init();
+			} catch (UnknownHostException | SocketException e) {
+				e.printStackTrace();
+			}
+		}
+		return hostName;
+	}
+	
 
 	/**
-	 * 获取本机ip地址，并自动区分Windows还是linux操作系统
 	 * linux 如若想用InetAddress.getLocalHost()方式获取本机ip，需要保证以下一点
 	 * '/etc/hosts'配置中 有一行配置 
-	 *                 127.0.0.1 主机名
-	 *  centOS主机名在'/etc/sysconfig/network' 配置中可以找到
-	 *  Ubuntu主机名在'/etc/network/interfaces' 配置中可以找到
+	 *                 			127.0.0.1  主机名
+	 *  centOS主机名在'/etc/sysconfig/network' 	配置中可以找到
+	 *  Ubuntu主机名在'/etc/network/interfaces' 	配置中可以找到
 	 *  不然就只能采取现提供的方法
 	 * @return String
 	 */
-	public static String getLocalIP() {
-		String sIP = "";
-		InetAddress ip = null;
-		try {
-			// 如果是Windows操作系统
-			if (isWindowsOS()) {
-				ip = InetAddress.getLocalHost();
-			}
-			// 如果是Linux操作系统
-			else {
-				boolean bFindIP = false;
-				Enumeration<NetworkInterface> netInterfaces = (Enumeration<NetworkInterface>) NetworkInterface.getNetworkInterfaces();
-				while (netInterfaces.hasMoreElements()) {
-					if (bFindIP) {
-						break;
-					}
-					NetworkInterface ni = (NetworkInterface) netInterfaces.nextElement();
-					// 遍历所有ip
-					Enumeration<InetAddress> ips = ni.getInetAddresses();
-					while (ips.hasMoreElements()) {
-						ip = (InetAddress) ips.nextElement();
-						// 127.开头的都是lookback地址
-						if (ip.isSiteLocalAddress() && !ip.isLoopbackAddress() 
-								&& ip.getHostAddress().indexOf(":") == -1) {
-							bFindIP = true;
-							break;
-						}
-					}
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		if (null != ip) {
-			sIP = ip.getHostAddress();
-		}
-		return sIP;
-	}
-
-	private static boolean isWindowsOS() {
-		boolean isWindowsOS = false;
-		String osName = System.getProperty("os.name");
-		if (osName.toLowerCase().indexOf("windows") > -1) {
-			isWindowsOS = true;
-		}
-		return isWindowsOS;
-	}
+    private static InetAddress getLocalHost() throws UnknownHostException, SocketException {
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        while (interfaces != null && interfaces.hasMoreElements()) {
+            Enumeration<InetAddress> addresses = interfaces.nextElement().getInetAddresses();
+            while (addresses != null && addresses.hasMoreElements()) {
+                InetAddress address = addresses.nextElement();
+                if (acceptableAddress(address)) {
+                    return address;
+                }
+            }
+        }
+        throw new UnknownHostException();
+    }
+    
+    private static boolean acceptableAddress(InetAddress address) {
+        return address != null && !address.isLoopbackAddress() && !address.isAnyLocalAddress() && !address.isLinkLocalAddress();
+    }
 
 	public static void main(String[] args) {
-		System.out.println(getLocalIP());
+		System.out.println(getLocalIp());
+		System.out.println(getLocalHostName());
 	}
 
 }
