271abd30636364f5748f4ed82b92ac0a6ebcca55
love-somnus <928200207@qq.com>
2018-06-03 14:43:33 +0800

    love-somnus <928200207@qq.com>

--- src/main/java/com/somnus/thread/pool/Callback.java
+++ src/main/java/com/somnus/thread/pool/Callback.java
@@ -1,16 +1,18 @@
 package com.somnus.thread.pool;
 
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
-public class CallableAndFuture {
+import org.junit.Test;
+
+public class Callback {
 	
-	public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
+	@Test
+	public void  futureTask() throws Exception {
 		/* 创建单个线程的线程池，如果当前线程在执行任务时突然中断，则会创建一个新的线程替代它继续执行任务 */
 		ExecutorService executor = Executors.newSingleThreadExecutor();
 
@@ -36,6 +38,35 @@ public class CallableAndFuture {
 		int result = future.get(5000, TimeUnit.MILLISECONDS); //取得结果，同时设置超时执行时间为5s
 		
 		System.out.println(result);
+		
+		executor.shutdown();// 任务执行完毕，关闭线程池
+	}
+	
+	@Test
+	public void future() throws Exception {
+		/* 创建单个线程的线程池，如果当前线程在执行任务时突然中断，则会创建一个新的线程替代它继续执行任务 */
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+
+		Future<Integer> future = executor.submit(() -> {
+			int sum = 0;
+			for (int j = 1; j <= 10; j++) {
+				try {
+					TimeUnit.MILLISECONDS.sleep(new Random().nextInt(10));// 为了测试出效果，让每次任务执行都需要一定时间
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println(Thread.currentThread().getName() + " is looping of " + j);
+				sum += j;
+			}
+			return sum;
+		});
+
+		System.out.println("你走你的，我先走一步");
+		Thread.sleep(10000);//模拟业务逻辑也在做自己的事情，同时开工
+		
+		int result = future.get(5000, TimeUnit.MILLISECONDS); //取得结果，同时设置超时执行时间为5s
+		
+		System.out.println(result);//
 		
 		executor.shutdown();// 任务执行完毕，关闭线程池
 	}
--- src/main/java/com/somnus/thread/pool/ExecutorCompletion.java
+++ src/main/java/com/somnus/thread/pool/ExecutorCompletion.java
@@ -8,6 +8,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Test;
+
 /**
  * 与ExecutorService一样，CompletionService也可以提交异步任务，它的不同是，它可以按任务完成顺序获取结果
  * @ClassName:     ExecutorCompletion.java
@@ -19,8 +21,9 @@ import java.util.concurrent.TimeUnit;
  */
 public class ExecutorCompletion {
 	
-	public static void main(String[] args) throws InterruptedException {
-        ExecutorService executor = Executors.newFixedThreadPool(5);
+	@Test
+	public void main() throws InterruptedException {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
   
         // 构建完成服务  
         CompletionService<Integer> completionService = new ExecutorCompletionService<Integer>(executor);
@@ -61,6 +64,48 @@ public class ExecutorCompletion {
   
         // 所有任务已经完成,关闭线程池  
         System.out.println("执行完毕....");
+        executor.shutdown();  
+	}
+	
+	@Test
+	public void sum() throws InterruptedException {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+  
+        // 构建完成服务  
+        CompletionService<Long> completionService = new ExecutorCompletionService<Long>(executor);
+        
+        final int groupNum = 1000000/100;
+        
+        for (int i = 1; i <= 100; i++){
+        	final int start = (i -1) * groupNum +1;
+			final int end = i * groupNum;
+    		completionService.submit(() -> {
+    			Long sum = 0L;
+				for (int j = start; j <= end; j++) {
+					System.out.println(Thread.currentThread().getName() + " is looping of " + j + " for start of " + start + " for end of " + end);
+					sum += j;
+				}
+				return sum;
+    		});//返回结果类型FutureTask  
+        }
+		
+		System.out.println("你走你的，我先走一步");
+		Thread.sleep(10000);//模拟业务逻辑也在做自己的事情，同时开工
+		
+		long result = 0L;
+  
+        for (int i = 0; i < 100; i++) {
+            try {  
+                result += completionService.take().get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+  
+        // 所有任务已经完成,关闭线程池  
+        System.out.println("执行完毕...." + result);
         executor.shutdown();  
 	}
 
