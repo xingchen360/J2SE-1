ac9d2ce0a59fcd05fd84d214d7e1f0bc4eea423d
love-somnus <928200207@qq.com>
2018-08-04 15:23:54 +0800

    自定义线程池

--- src/main/java/com/somnus/thread/pool/ThreadPool.java
+++ src/main/java/com/somnus/thread/pool/ThreadPool.java
@@ -1,12 +1,17 @@
 package com.somnus.thread.pool;
 
 import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 public class ThreadPool {
 	
@@ -113,5 +118,41 @@ public class ThreadPool {
         cdl.await();
         executor.shutdown();// 任务执行完毕，关闭线程池
 	}
+	
+	@Test
+	public void userdefinedThreadPool() throws InterruptedException {
+		/*返回一个可根据实际情况调整线程个数的线程池，不限制最大线程数量，若用空闲的线程则执行任务，
+		 * 若无任务则不创建线程。并且每一个空闲线程会在60s后自动回收*/
+		ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("consumer-queue-thread-%d").build();
+
+		ExecutorService executor = new ThreadPoolExecutor(5, 5, 0L, TimeUnit.MILLISECONDS,
+				new ArrayBlockingQueue<Runnable>(5), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
+        
+		for (int i = 1; i <=10; i++){
+            final int taskID = i;
+            executor.execute(() -> {
+            	for (int j = 1; j <= 10; j++){
+                    try{
+                    	TimeUnit.MILLISECONDS.sleep(new Random().nextInt(1000));// 为了测试出效果，让每次任务执行都需要一定时间
+                    }
+                    catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName() + " is looping of " + j + " for task of " + taskID);
+                }
+            	cdl.countDown();
+            });
+        }
+        /** 
+         * shutdown 允许之前已经提交但未执行或未完成的任务继续完成它，平滑的关闭ExecutorService，
+         * 当此方法被调用时，ExecutorService停止接收新的任务并且等待已经提交的任务（包含提交正在执行和提交未执行）执行完成。
+         * 当所有提交任务执行完毕，线程池即被关闭。
+         * 
+         * shutdownNow 阻止已经提交(但尚未运行的)的任务运行并且尝试停止正在运行的任务
+         */
+        cdl.await();
+        executor.shutdown();// 任务执行完毕，关闭线程池
+	}
+
 
 }
